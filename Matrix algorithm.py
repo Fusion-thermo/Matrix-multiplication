866cb73e00555a7c89d927796d2af9072f181175
Jean BERTHAUD <jean.berthaud@outlook.com>
2023-05-22 23:56:45 +0200

    works with -1,0,1
    Works for n=2, k=8 but not K=7 (strassen)

--- Matrix algorithm.py
+++ Matrix algorithm.py
@@ -1,6 +1,6 @@
 import numpy as np
 from pyomo.environ import ConcreteModel, Var, Objective, SolverFactory, ConstraintList, RangeSet, Constraint
-from pyomo.environ import PositiveIntegers,Binary, Integers
+from pyomo.environ import PositiveIntegers,Binary, Integers, NonNegativeIntegers
 
 # a=np.array([[0,2],
 #             [1,1]])
@@ -21,6 +21,7 @@ def basic_multiplication_alogrithm(n):
             indice+=1
             a[i,j]=indice
     b=np.copy(a)
+    #the first dimension of c is the number of the cell : 1,2,...n**2
     for i in range(n):
         for j in range(n):
             for k in range(n):
@@ -33,17 +34,17 @@ def basic_multiplication_alogrithm(n):
 #c=basic_multiplication_alogrithm(2)
 #print(c)
 
-def resolution(n):
+def resolution(n,k):
 
     model = ConcreteModel()
 
-    K=n**3
     model.I=RangeSet(1,n**2)
-    model.M=RangeSet(1,K)
-    model.u=Var(model.I, model.M,within=Binary)
-    model.v=Var(model.I, model.M,within=Binary)
-    model.w=Var(model.I, model.M,within=Binary)
-    model.ab=Var(model.I,model.I,model.M,within=Integers)
+    model.M=RangeSet(1,k)
+    model.u=Var(model.I, model.M,within={-1,0,1})
+    model.v=Var(model.I, model.M,within={-1,0,1})
+    model.w=Var(model.I, model.M,within={-1,0,1})
+    model.ab=Var(model.I,model.I,model.M,within={-1,0,1})
+    #model.m=Var(model.M,within=NonNegativeIntegers)
     #model.c=Var(model.I,model.I,model.I,within=Binary)
     c=basic_multiplication_alogrithm(n)
     """ #if i convert c in a variable it will get modified to full 0
@@ -61,18 +62,20 @@ def resolution(n):
     
     #we want to reduce the number of additions for a given K ==> reduce non-zero terms
     #absolute value for -1, 0, 1 : can be cell squared
-    #for binary
-    model.obj = Objective(expr=sum(sum(model.u[i,m] for m in model.M) for i in model.I) + sum(sum(model.v[i,m] for m in model.M) for i in model.I) + sum(sum(model.w[i,m] for m in model.M) for i in model.I))
-    #for -1,0,1 :
-    #model.obj = Objective(expr=sum(sum(model.u[i,m]*model.u[i,m] for m in model.M) for i in model.I) + sum(sum(model.v[i,m]*model.v[i,m] for m in model.M) for i in model.I) + sum(sum(model.w[i,m]*model.w[i,m] for m in model.M) for i in model.I))
+    #1-norm for binary
+    #model.obj = Objective(expr=sum(sum(model.u[i,m] for m in model.M) for i in model.I) + sum(sum(model.v[i,m] for m in model.M) for i in model.I) + sum(sum(model.w[i,m] for m in model.M) for i in model.I))
+    #1-norm for -1,0,1 :
+    model.obj = Objective(expr=sum(sum(model.u[i,m]*model.u[i,m] for m in model.M) for i in model.I) + sum(sum(model.v[i,m]*model.v[i,m] for m in model.M) for i in model.I) + sum(sum(model.w[i,m]*model.w[i,m] for m in model.M) for i in model.I))
     
+    #new formula must be equivalent to the basic formula with n**3 multiplication
     model.sum=ConstraintList()
     for h in model.I:
         for i in model.I:
             for j in model.I:
-                #model.sum.add(expr=sum(model.w[h,k] * model.u[i,k] * model.v[j,k] for k in model.M) == model.c[i,j,h])
+                #model.sum.add(expr=sum(model.w[h,k] * model.u[i,k] * model.v[j,k] for k in model.M) == model.c[i,j,h]) #body with non linear terms
                 model.sum.add(expr=sum(model.w[h,k] * model.ab[i,j,k] for k in model.M) == c[h-1,i-1,j-1])
 
+    #in order to reduce the above constraint in a quadratic form, we describe here the multiplication of each aibj for each k
     model.product=ConstraintList()
     for i in model.I:
         for j in model.I:
@@ -80,16 +83,23 @@ def resolution(n):
                 model.product.add(expr=model.ab[i,j,k] == model.u[i,k] * model.v[j,k])
 
 
+    #this represents the number of multiplication : each non zero cell counts for one multiplication
+    # model.multipl=ConstraintList()
+    # for k in model.M:
+    #     model.multpli.add(expr=model.m[k] == sum(w[i,k]*w[i,k] for i in model.I))
+
     # Write the LP model in standard format
     #model.write("matrix_{}.lp".format(n))
 
     # Solve the model
-    sol = SolverFactory('gurobi').solve(model, tee=True)
-    #sol = SolverFactory('gurobi').solve(model, tee=True, options_string="NonConvex=2")
+    #sol = SolverFactory('gurobi').solve(model, tee=True)
+    sol = SolverFactory('gurobi').solve(model, tee=True, options={"NonConvex":2,"TimeLimit":45*60})
+    #the solver found a feasible solution if there is an "incumbent" value
 
     return model.u, model.v, model.w
 
 def algo_verification(u,v,w,n):
+    #verifies if the algorithm works
     for test in range(10):
         a=np.random.randint(-10,10,(n,n))
         b=np.random.randint(-10,10,(n,n))
@@ -110,17 +120,19 @@ def algo_verification(u,v,w,n):
 
 
 n=2
-u,v,w=resolution(n)
-for i in range(1,5):
-    print([int(u[i,m]()) for m in range(1,9)])
+k=n**3-1
+u,v,w=resolution(n,k)
+I=RangeSet(1,n**2)
+M=RangeSet(1,k)
+for i in I:
+    print([int(u[i,m]()) for m in M])
 print("\n")
-for i in range(1,5):
-    print([int(v[i,m]()) for m in range(1,9)])
+for i in I:
+    print([int(v[i,m]()) for m in M])
 print("\n")
-for i in range(1,5):
-    print([int(w[i,m]()) for m in range(1,9)])
+for i in I:
+    print([int(w[i,m]()) for m in M])
 print("\n")
-
 
 if algo_verification(u,v,w,n):
     print("Functional")
