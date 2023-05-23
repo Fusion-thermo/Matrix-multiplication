246399f9abb6d8c9fd72396fadfed0843fd6b877
Jean BERTHAUD <jean.berthaud@outlook.com>
2023-05-23 12:12:39 +0200

    Cleaner but no improvement

--- Matrix algorithm.py
+++ Matrix algorithm.py
@@ -22,6 +22,7 @@ def basic_multiplication_alogrithm(n):
             a[i,j]=indice
     b=np.copy(a)
     #the first dimension of c is the number of the cell : 1,2,...n**2
+    #pyomo : line, column, 3D ; numpy : 3D, line, column
     for i in range(n):
         for j in range(n):
             for k in range(n):
@@ -44,38 +45,31 @@ def resolution(n,k):
     model.v=Var(model.I, model.M,within={-1,0,1})
     model.w=Var(model.I, model.M,within={-1,0,1})
     model.ab=Var(model.I,model.I,model.M,within={-1,0,1})
-    #model.m=Var(model.M,within=NonNegativeIntegers)
-    #model.c=Var(model.I,model.I,model.I,within=Binary)
+    model.m=Var(model.M,within=NonNegativeIntegers)
     c=basic_multiplication_alogrithm(n)
-    """ #if i convert c in a variable it will get modified to full 0
-    for i in range(n**2):
-        for j in range(n**2):
-            for k in range(n**2):
-                model.c[j+1,k+1,i+1] = c[i,j,k] #pyomo : line, column, 3D ; numpy : 3D, line, column
-    #display
-    for k in range(n**2):
-        for i in range(n**2):
-            l=[]
-            for j in range(n**2):
-                l.append(c[i+1,j+1,k+1]())
-            print(l) """
     
     #we want to reduce the number of additions for a given K ==> reduce non-zero terms
     #absolute value for -1, 0, 1 : can be cell squared
     #1-norm for binary
     #model.obj = Objective(expr=sum(sum(model.u[i,m] for m in model.M) for i in model.I) + sum(sum(model.v[i,m] for m in model.M) for i in model.I) + sum(sum(model.w[i,m] for m in model.M) for i in model.I))
-    #1-norm for -1,0,1 :
-    model.obj = Objective(expr=sum(sum(model.u[i,m]*model.u[i,m] for m in model.M) for i in model.I) + sum(sum(model.v[i,m]*model.v[i,m] for m in model.M) for i in model.I) + sum(sum(model.w[i,m]*model.w[i,m] for m in model.M) for i in model.I))
-    
+    #1-norm for -1,0,1. n=2,k=8 : incumbent found in 24 seconds, lb=30 additions
+    #model.obj = Objective(expr=sum(sum(model.u[i,m]*model.u[i,m] for m in model.M) for i in model.I) + sum(sum(model.v[i,m]*model.v[i,m] for m in model.M) for i in model.I) + sum(sum(model.w[i,m]*model.w[i,m] for m in model.M) for i in model.I))
+    #test : objective to reduce the number of multiplication. n=2,k=8 : incumbent found in 7 seconds
+    model.obj = Objective(expr=sum(k*model.m[k] for k in model.M))
+
+
     #new formula must be equivalent to the basic formula with n**3 multiplication
+    #basically, the number of products aibj for a given ch must be the same than the standard matrix algorithm.
+    #for example, if c1 uses one time a1b1 and both M1 and M2 contain one a1b1 each, then c1 = 2*M1 - M2 is ok (only for product a1b1)
+    #h : line of w (c), i : line of u, j : line of v
     model.sum=ConstraintList()
     for h in model.I:
         for i in model.I:
             for j in model.I:
-                #model.sum.add(expr=sum(model.w[h,k] * model.u[i,k] * model.v[j,k] for k in model.M) == model.c[i,j,h]) #body with non linear terms
+                #model.sum.add(expr=sum(model.w[h,k] * model.u[i,k] * model.v[j,k] for k in model.M) == c[h-1,i-1,j-1]) #body with non linear terms
                 model.sum.add(expr=sum(model.w[h,k] * model.ab[i,j,k] for k in model.M) == c[h-1,i-1,j-1])
 
-    #in order to reduce the above constraint in a quadratic form, we describe here the multiplication of each aibj for each k
+    #in order to reduce the above constraint in a quadratic form, we store here the multiplication of each aibj for each k
     model.product=ConstraintList()
     for i in model.I:
         for j in model.I:
@@ -84,16 +78,16 @@ def resolution(n,k):
 
 
     #this represents the number of multiplication : each non zero cell counts for one multiplication
-    # model.multipl=ConstraintList()
-    # for k in model.M:
-    #     model.multpli.add(expr=model.m[k] == sum(w[i,k]*w[i,k] for i in model.I))
+    model.multipl=ConstraintList()
+    for k in model.M:
+        model.multipl.add(expr=model.m[k] == sum(model.w[i,k]*model.w[i,k] for i in model.I))
 
     # Write the LP model in standard format
     #model.write("matrix_{}.lp".format(n))
 
     # Solve the model
     #sol = SolverFactory('gurobi').solve(model, tee=True)
-    sol = SolverFactory('gurobi').solve(model, tee=True, options={"NonConvex":2,"TimeLimit":45*60})
+    sol = SolverFactory('gurobi').solve(model, tee=True, options={"NonConvex":2,"TimeLimit":1*60})
     #the solver found a feasible solution if there is an "incumbent" value
 
     return model.u, model.v, model.w
